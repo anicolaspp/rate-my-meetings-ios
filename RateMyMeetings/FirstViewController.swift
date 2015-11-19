84085ce5042aaf39258c2aaeb05b19a36cd909cb
Nicolas A Perez <nperez@ipcoop.com>
2015-11-19 09:24:40 -0500

    First Commit

--- RateMyMeetings/FirstViewController.swift
+++ RateMyMeetings/FirstViewController.swift
@@ -7,9 +7,12 @@
 //
 
 import UIKit
+import EventKit
 
 class FirstViewController: UIViewController {
 
+    @IBOutlet weak var calendar: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,42 @@ class FirstViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func accessToCalendar(sender: AnyObject) {
+        
+        let store = EKEventStore()
+        store.requestAccessToEntityType(EKEntityType.Event) { (accessGranted, error) -> Void in
+        
+            if (accessGranted) {
+            
+                let calendar = NSCalendar.currentCalendar()
+                
+                let oneDayAgoComponnents = NSDateComponents()
+                oneDayAgoComponnents.day = -1
+            
+                let oneDayAgo = calendar.dateByAddingComponents(oneDayAgoComponnents, toDate: NSDate(), options: NSCalendarOptions.MatchNextTime)
+            
+                let oneYearFromNowComponnent = NSDateComponents()
+                oneYearFromNowComponnent.year = 1
+            
+                let oneYearFromNow = calendar.dateByAddingComponents(oneYearFromNowComponnent, toDate: NSDate(), options: NSCalendarOptions.MatchNextTime)
+            
+                let predicate = store.predicateForEventsWithStartDate(oneDayAgo!, endDate: oneYearFromNow!, calendars:  nil)
+                
+                let events = store.eventsMatchingPredicate(predicate)
+                
+                let firstTittle = events[0].title
+                
+                
 
+                let alertAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil)
+                let alertController = UIAlertController(title: "Alert", message: firstTittle, preferredStyle: UIAlertControllerStyle.Alert)
+                alertController.addAction(alertAction)
+                
+                
+                self.presentViewController(alertController, animated: true, completion: nil)
+            }
+            
+        }
+    }
 }
 
