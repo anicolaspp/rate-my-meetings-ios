b19c587aae779f5ad76778d9f666e6cdcee7ea4e
Nicolas A Perez <nperez@ipcoop.com>
2015-11-21 23:05:16 -0500

    adding login window

---
+++ RateMyMeetings/LoginViewController.swift
@@ -0,0 +1,85 @@
+//
+//  LoginViewController.swift
+//  RateMyMeetings
+//
+//  Created by Nicolas Perez on 11/21/15.
+//  Copyright © 2015 Nicolas Perez. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    
+    var userRepository: IUserRepository! = UserRepositoryStub()
+    var userId = 0
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func registerNewUser(sender: UIButton) {
+       
+    }
+
+    @IBAction func loginUser(sender: AnyObject) {
+        
+        let login = UIAlertController(title: "Login", message: "Enter Credentials", preferredStyle: .Alert)
+        
+        login.addTextFieldWithConfigurationHandler { (field: UITextField) -> Void in
+            
+            field.placeholder = "User Name"
+        }
+        
+        login.addTextFieldWithConfigurationHandler { (field) -> Void in
+            
+            field.placeholder = "Password"
+            field.secureTextEntry = true
+        }
+        
+        let ok = UIAlertAction(title: "Login", style: .Default) { (taction) -> Void in
+            let user = login.textFields?[0].text
+            let pass = login.textFields?[1].text
+            
+            self.userId = self.userRepository!.longin(user, password: pass)
+            
+            if (self.userId > 0) {
+                self.performSegueWithIdentifier("loginSegue", sender: self)
+            }
+            else {
+                self.showAlert("Error", message: "Invalid user or password", handler: nil)
+            }
+        }
+        
+        login.addAction(ok)
+        
+        self.presentViewController(login, animated: true, completion: nil)
+    }
+
+
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+//         Get the new view controller using segue.destinationViewController.
+//         Pass the selected object to the new view controller.
+        
+        print("here\n")
+        
+    }
+
+
+}
+
+extension UIViewController {
+    func showAlert(title: String, message: String, handler: ((UIAlertAction) -> Void)?) -> Void {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .Alert)
+        alert.addAction(UIAlertAction(title: "Close", style: UIAlertActionStyle.Default, handler: handler))
+        
+        self.presentViewController(alert, animated: true, completion: nil)
+    }
+}
