342cb03fa2ad6b5b4b106c57a15a3d112fd9925e
Nicolas A Perez <nperez@ipcoop.com>
2015-11-23 15:20:26 -0500

    Adding tabbed controllers

--- FindTeamsTableViewController.swift
+++ FindTeamsTableViewController.swift
@@ -19,7 +19,6 @@ class FindTeamsTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-
         let domain = teamName?.characters.split(isSeparator: { (Character) -> Bool in
             return Character == "@"
         })[1]
@@ -27,14 +26,7 @@ class FindTeamsTableViewController: UITableViewController {
         title = String(domain!)
         
         teams = companyRepository?.getTeamsWithDomain(title!)
-        // Load team for domain
-        
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
-     
+      
         self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .Add, target: self, action: Selector("addNewTeam"))
     }
 
@@ -66,41 +58,6 @@ class FindTeamsTableViewController: UITableViewController {
         return cell
     }
    
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
     override func shouldPerformSegueWithIdentifier(identifier: String, sender: AnyObject?) -> Bool {
         if (identifier == "showTeamMembersSegue") {
             if let _ = self.tableView.indexPathForSelectedRow?.row {
