a75ea15749486ecd12debb1774c2e9065ddab09c
Nicolas A Perez <nperez@ipcoop.com>
2015-11-24 13:31:38 -0500

    loading tab controller when login

--- RateMyMeetings/Extensions.swift
+++ RateMyMeetings/Extensions.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import UIKit
 
 extension String {
     func isValidEmail() -> Bool {
@@ -16,4 +17,45 @@ extension String {
         
         return emailTest.evaluateWithObject(self)
     }
+}
+
+extension UIViewController {
+    func showAlert(title: String, message: String, handler: ((UIAlertAction) -> Void)?) -> Void {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .Alert)
+        alert.addAction(UIAlertAction(title: "Close", style: UIAlertActionStyle.Default, handler: handler))
+        
+        self.presentViewController(alert, animated: true, completion: nil)
+    }
+    
+    func executeAsyncWithIndicator(activityIndicator: UIActivityIndicatorView, action: () -> AnyObject?, completion: (result: AnyObject?) -> Void) {
+        showActivityIndicatory(activityIndicator, uiView: self.view)
+        
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), { () -> Void in
+            
+            let result = action()
+            
+            dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                activityIndicator.stopAnimating()
+                self.view.userInteractionEnabled = true
+                
+                completion(result: result)
+            })
+        })
+    }
+    
+    
+    func showActivityIndicatory(activityIndicator: UIActivityIndicatorView, uiView: UIView) {
+        
+        activityIndicator.frame = CGRectMake(0.0, 0.0, 40.0, 40.0);
+        activityIndicator.center = uiView.center
+        activityIndicator.hidesWhenStopped = true
+        activityIndicator.activityIndicatorViewStyle = .Gray
+        activityIndicator.backgroundColor = UIColor.whiteColor().colorWithAlphaComponent(0.1)
+        
+        self.view.userInteractionEnabled = false
+        
+        activityIndicator.startAnimating()
+        uiView.addSubview(activityIndicator)
+    }
+    
 }
--- RateMyMeetings/LoginViewController.swift
+++ RateMyMeetings/LoginViewController.swift
@@ -8,11 +8,12 @@
 
 import UIKit
 
+
+
 class LoginViewController: UIViewController {
     
     var userRepository = UserRepositoryStub()
     var userId = 0
-    var loggedUser: User?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,21 +33,21 @@ class LoginViewController: UIViewController {
             let registrationController = target.viewControllers.first as! UserVerificationViewController
             registrationController.userRepository = self.userRepository
         
-            print("here\n")
-        }
-        
-        if (segue.identifier == "loginSegue") {
-            let target = (segue.destinationViewController as! UITabBarController).viewControllers?.first as! UINavigationController
-            let main = target.viewControllers.first as! FirstViewController
-            
-            main.loggedUser = loggedUser
+    //        print("here\n")
         }
     }
     
     @IBAction func loginUser(sender: AnyObject) {
         
-        let loginForm = getLoginForm()
-        self.presentViewController(loginForm, animated: true, completion: nil)
+       let form = getLoginForm()
+       self.presentViewController(form, animated: true, completion: nil)
+    }
+    
+    func loadMainAppPageFor(user: User) {
+        
+        let tabController = self.storyboard?.instantiateViewControllerWithIdentifier("mainTabController")
+        
+        self.presentViewController(tabController!, animated: true, completion: nil)
     }
     
     func getLoginForm() -> UIAlertController {
@@ -68,61 +69,27 @@ class LoginViewController: UIViewController {
             let user = login.textFields?[0].text
             let pass = login.textFields?[1].text
             
-            if let user = self.userRepository.longin(user, password: pass) {
-                self.loggedUser = user
-                self.performSegueWithIdentifier("loginSegue", sender: self)
-            }
-            else {
-                self.showAlert("Error", message: "Invalid user or password", handler: nil)
-            }
+            self.executeAsyncWithIndicator(UIActivityIndicatorView(),
+                action: { () -> AnyObject? in
+                    
+                    let user = self.userRepository.longin(user, password: pass)
+                    
+                    return user
+
+                }, completion: { (result) -> Void in
+                    
+                    if let user = result as? User {
+                        self.loadMainAppPageFor(user)
+                    }
+                    else {
+                        self.showAlert("Error", message: "Invalid user or password", handler: nil)
+                    }
+            })
         }
         
         login.addAction(UIAlertAction(title: "Cancel", style: .Cancel, handler: nil))
         login.addAction(ok)
         
         return login
-    }
-}
-
-
-extension UIViewController {
-    func showAlert(title: String, message: String, handler: ((UIAlertAction) -> Void)?) -> Void {
-        let alert = UIAlertController(title: title, message: message, preferredStyle: .Alert)
-        alert.addAction(UIAlertAction(title: "Close", style: UIAlertActionStyle.Default, handler: handler))
-        
-        self.presentViewController(alert, animated: true, completion: nil)
-    }
-}
-
-extension UIViewController {
-
-    func executeAsyncWithIndicator(activityIndicator: UIActivityIndicatorView, action: () -> AnyObject?, completion: (result: AnyObject?) -> Void) {
-        showActivityIndicatory(activityIndicator, uiView: self.view)
-        
-        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), { () -> Void in
-          
-            let result = action()
-            
-            dispatch_async(dispatch_get_main_queue(), { () -> Void in
-                activityIndicator.stopAnimating()
-                self.view.userInteractionEnabled = true
-                
-                completion(result: result)
-            })
-        })
-    }
-    
-    func showActivityIndicatory(activityIndicator: UIActivityIndicatorView, uiView: UIView) {
-        
-        activityIndicator.frame = CGRectMake(0.0, 0.0, 40.0, 40.0);
-        activityIndicator.center = uiView.center
-        activityIndicator.hidesWhenStopped = true
-        activityIndicator.activityIndicatorViewStyle = .Gray
-        activityIndicator.backgroundColor = UIColor.whiteColor().colorWithAlphaComponent(0.1)
-        
-        self.view.userInteractionEnabled = false
-        
-        activityIndicator.startAnimating()
-        uiView.addSubview(activityIndicator)
     }
 }
