d35aeab1deb7b39759d073c55d9aa4c6e9d7be27
Nicolas A Perez <nperez@ipcoop.com>
2015-11-27 11:26:26 -0500

    Adding custom tableviewcell

---
+++ RateMyMeetings/MeetingTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MeetingTableViewCell.swift
+//  RateMyMeetings
+//
+//  Created by Nicolas Perez on 11/26/15.
+//  Copyright © 2015 Nicolas Perez. All rights reserved.
+//
+
+import UIKit
+
+class MeetingTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
